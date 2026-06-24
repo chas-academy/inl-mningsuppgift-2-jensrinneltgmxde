fc1febc8a3944227256a240b3d05463cc8adbb20
jens.rinneltgmx.de <jens.rinnelt@gmx.de>
2026-06-24 11:53:27 +0200

    klar

--- src/script.js
+++ src/script.js
@@ -1,26 +1,39 @@
 // Definera array av namn
-let people;
+let people = ["Jens", "Lena", "Gustav", "Linnea", "Ute", "Michael", "Maria", "Heinz", "Petra", "Jana"];
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
-  
+  const searchInput = document.getElementById("searchInput")
+  const searchBtn = document.getElementById("searchBtn")
+  const resultDisplay = document.getElementById("resultDisplay")
+
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
-    
+    const result = searchInput.value
+
     // 2. Skapa en boolean för found
-    
+    let found = false
+
     // 3. Loopa igenom people-arrayen
-    
+    for (let person of people) {
+
     // 4. Jämför texten med varje namn i arrayen
-    
+      if (result === person) {
+
     // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
-    // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
+    resultDisplay.innerHTML = "Namn hittades: " + result
+    found = true  
+
+    // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen. 
+      } else if (!found) {
+        resultDisplay.innerHTML = "Namn hittades inte."
+        found = false
+      }
+    }   
     });
 }
 
